4c354bedec78b524b7e5a0083249e29b1a575fd7
arnolgarzon <arnol_garzon@hotmail.com>
2022-01-13 23:17:54 -0500

    valor 2.0

--- precioventa/valor.js
+++ precioventa/valor.js
@@ -1,26 +1,57 @@
-let inputCat = document.querySelector('#inputCat');
+let inputCategory = document.querySelector('#inputCat');
 let inputValue = document.querySelector('#inputValue');
 let inputName = document.querySelector('#inputName');
 let inputWeight= document.querySelector('#inputWeight');
 let buttonSend = document.querySelector('#buttonSend');
 let envio = 10;
-let peso = (parseInt(inputWeight.value));
-let iva = ((envio + inputValue.value));
+let value;
+let peso;
+let iva;
+let arancel;
+let comision;
+let valueEnd;
 
-function calcularComision(){
+function calcularPrecio(){
+    peso = (parseInt(inputWeight.value));
+    value = (parseInt(inputValue.value));
+    arancel = (value * 0.10);
+
     if(peso <= 4){
-        alert("envio = $10")
-
+        alert("el costo del envio es: $" + envio);
     }
     else if (peso > 4){
         for (let i = 0; i < (peso - 4); i++){
         envio += 2
         }
+        alert("el costo del envio es: $" + envio);
     }
-    if (inputValue.value > 200){
-        alert("el iva es: " + iva) 
+    if (value > 200){
+        iva = ((envio + value + arancel) * 0.19);
+        alert("el iva es de: " + iva);
     }
-   alert("el costo del envio es: $" + envio);
+    if (inputCategory.value == 1){
+        comision = 0;
+    }
+    else if (inputCategory == 2){
+        commission = 0.1;
+    }
+    else if (inputCategory == 3){
+        comision = 0.05;
+    }
+    else if (inputCategory == 4){
+        comision = 0.15
+    }
+    if (comision != 0){
+        valueEnd = (envio + value + arancel);
+    }
+    else if (value > 200){
+        valueEnd = ((envio + value + arancel + iva) * (comision / (1 - comision)));
+    }
+    else{
+        valueEnd = (((envio + value + arancel) * comision) / (1 - comision));
+    }
+    document.querySelector('#producto').innerHTML = inputName.value;
+    document.querySelector('#valorFinal').innerHTML = valueEnd;
 }
 
-buttonSend.addEventListener('click',calcularComision);
+buttonSend.addEventListener('click',calcularPrecio);
