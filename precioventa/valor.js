ef08de5a4cb5b16b4c123f6774c038e8d5b356a2
arnolgarzon <arnol_garzon@hotmail.com>
2022-01-17 14:31:39 -0500

    valor version 3.0

--- precioventa/valor.js
+++ precioventa/valor.js
@@ -9,49 +9,61 @@ let peso;
 let iva;
 let arancel;
 let comision;
+let categoria;
 let valueEnd;
+let nombre = document.querySelector('#producto');
 
 function calcularPrecio(){
-    peso = (parseInt(inputWeight.value));
+    
     value = (parseInt(inputValue.value));
     arancel = (value * 0.10);
+    iva = ((envio + value + arancel) * 0.19);
+    categoria = inputCategory.value;
 
-    if(peso <= 4){
-        alert("el costo del envio es: $" + envio);
+    calcularCostoEnvio();
+    //operador ternario
+     (value > 200) ? alert("el iva es de: " + iva) : alert("no tiene iva");
+
+    // switch case
+    switch (categoria){
+        case 1 : comision = 0
+        break
+        case 2 : comision = 0.10
+        break
+        case 3 : comision = 0.05
+        break
+        case 4 : comision = 0.15
+        break
+        default : console.log("Invalid categoria")
+        break
     }
-    else if (peso > 4){
+    
+    if (comision == 0 & value < 200){
+        valueEnd = (envio + value + arancel)*1.1;
+    }
+    else if (comision == 0 & value > 200){
+        valueEnd = ((envio + value + arancel + iva) * 1.1 )
+    }
+    else if (value > 200){
+        valueEnd = ((envio + value + arancel + iva) * 1.1 ) / (1 - comision);
+    }
+    else{
+        valueEnd = ((envio + value + arancel) * 1.1 ) / (1 - comision);
+    }
+    nombre.innerHTML = inputName.value;
+    alert(valueEnd)
+}
+function calcularCostoEnvio (){
+    peso = (parseInt(inputWeight.value));
+    if(peso > 4){
         for (let i = 0; i < (peso - 4); i++){
         envio += 2
         }
         alert("el costo del envio es: $" + envio);
     }
-    if (value > 200){
-        iva = ((envio + value + arancel) * 0.19);
-        alert("el iva es de: " + iva);
-    }
-    if (inputCategory.value == 1){
-        comision = 0;
-    }
-    else if (inputCategory == 2){
-        commission = 0.1;
-    }
-    else if (inputCategory == 3){
-        comision = 0.05;
-    }
-    else if (inputCategory == 4){
-        comision = 0.15
-    }
-    if (comision != 0){
-        valueEnd = (envio + value + arancel);
-    }
-    else if (value > 200){
-        valueEnd = ((envio + value + arancel + iva) * (comision / (1 - comision)));
-    }
     else{
-        valueEnd = (((envio + value + arancel) * comision) / (1 - comision));
-    }
-    document.querySelector('#producto').innerHTML = inputName.value;
-    document.querySelector('#valorFinal').innerHTML = valueEnd;
+        alert("el costo del envio es: " + envio)
+    } 
 }
 
 buttonSend.addEventListener('click',calcularPrecio);
