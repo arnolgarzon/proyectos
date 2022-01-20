fabc860706c352f21841a9eeb24da21de0912c32
arnolgarzon <arnol_garzon@hotmail.com>
2022-01-19 23:06:32 -0500

    valor 3.0

--- precioventa/valor.js
+++ precioventa/valor.js
@@ -11,31 +11,36 @@ let arancel;
 let comision;
 let categoria;
 let valueEnd;
-let nombre = document.querySelector('#producto');
+let nombreHtml = document.querySelector('#producto');
+let valorHtml = document.querySelector('#valor');
 
 function calcularPrecio(){
     
     value = (parseInt(inputValue.value));
     arancel = (value * 0.10);
     iva = ((envio + value + arancel) * 0.19);
-    categoria = inputCategory.value;
+    categoria = (parseInt(inputCategory.value));
 
     calcularCostoEnvio();
     //operador ternario
      (value > 200) ? alert("el iva es de: " + iva) : alert("no tiene iva");
 
     // switch case
-    switch (categoria){
-        case 1 : comision = 0
+    switch (categoria) {
+        case 1 : 
+        comision = 0
         break
-        case 2 : comision = 0.10
+        case 2 : 
+        comision = 0.10
         break
-        case 3 : comision = 0.05
+        case 3 : 
+        comision = 0.05
         break
-        case 4 : comision = 0.15
+        case 4 : 
+        comision = 0.15
         break
-        default : console.log("Invalid categoria")
-        break
+        default : 
+        console.log("Invalid categoria")
     }
     
     if (comision == 0 & value < 200){
@@ -50,8 +55,8 @@ function calcularPrecio(){
     else{
         valueEnd = ((envio + value + arancel) * 1.1 ) / (1 - comision);
     }
-    nombre.innerHTML = inputName.value;
-    alert(valueEnd)
+    nombreHtml.innerHTML = "Tu producto es: " + inputName.value;
+    valorHtml.innerHTML = "el costo total del envio es: " + valueEnd;
 }
 function calcularCostoEnvio (){
     peso = (parseInt(inputWeight.value));
@@ -62,7 +67,7 @@ function calcularCostoEnvio (){
         alert("el costo del envio es: $" + envio);
     }
     else{
-        alert("el costo del envio es: " + envio)
+        alert("El costo del envio es: " + envio)
     } 
 }
 
